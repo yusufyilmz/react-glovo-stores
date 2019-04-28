33db1835b7e67e648316da2bd85db2fdf6eacc9c
yusufyilmz <yusufyilmz@yusufs-MacBook-Pro.local>
2019-04-28 21:19:52 +0300

    fixed some bugs

--- client/src/actions/category-actions.js
+++ client/src/actions/category-actions.js
@@ -13,7 +13,7 @@ export const fetchCategories = () => async (dispatch, getState) => {
       dispatch(success(storeStateHelper.getCategoryStoreStates(data)));
     })
     .catch((error) => {
-      //   dispatch(fail(ERROR_OCCURED));
+        dispatch(fail(ERROR_OCCURED));
     });
 
   function success(data) { return { type: actionTypes.FETCH_CATEGORIES_SUCCESS, payload: data } }
