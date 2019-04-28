e12872f8a51e2bf3e34627ed5eef8b1f659f3bad
yusufyilmz <yusufyilmz@yusufs-MacBook-Pro.local>
2019-04-28 19:55:09 +0300

    added client build

---
+++ client/build/precache-manifest.a08a6777a4fa2d7da0afc789cd191333.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f479227b0ee61e36418",
+    "url": "/static/js/runtime~main.c928898d.js"
+  },
+  {
+    "revision": "98e3401483f52d739217",
+    "url": "/static/js/main.e17e1eba.chunk.js"
+  },
+  {
+    "revision": "927e75129fe58d2c9286",
+    "url": "/static/js/6.ac42d3a8.chunk.js"
+  },
+  {
+    "revision": "ffcb46029cf53b1f6abf",
+    "url": "/static/js/5.2fb8970d.chunk.js"
+  },
+  {
+    "revision": "dcc88130cbfec94a0fb6",
+    "url": "/static/js/4.314e629d.chunk.js"
+  },
+  {
+    "revision": "aed69dd62572a058d5c4",
+    "url": "/static/js/3.400743f3.chunk.js"
+  },
+  {
+    "revision": "c44c925cba44ffda5463",
+    "url": "/static/js/0.e7330563.chunk.js"
+  },
+  {
+    "revision": "c8056d766ae357117898b224c19f8717",
+    "url": "/index.html"
+  }
+];
---
+++ client/build/static/js/4.314e629d.chunk.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{85:function(e,n,t){"use strict";t.r(n),t.d(n,"default",function(){return i});var o=t(0),a=t.n(o),c=t(12);function i(e){return a.a.createElement(c.b,{variant:"top",src:e.item.closed?e.item.sleepIcon:e.item.openIcon,alt:"Category ".concat(e.item.label," image")})}}}]);
+//# sourceMappingURL=4.314e629d.chunk.js.map
